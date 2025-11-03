fbc0c8c6392b93cb1635d495493ee21acfbc826c
jo19lune <joachimloick939@gmail.com>
2025-11-03 11:07:37 +0300

    dico

---
+++ interface/core/config.py
@@ -0,0 +1,9 @@
+# Dictionnaire des extensions classées par catégorie
+EXTENSIONS = {
+    "images": ["jpg", "jpeg", "png", "gif", "bmp", "svg", "webp"],
+    "videos": ["mp4", "mkv", "mov", "avi", "flv", "wmv", "3gp"],
+    "audios": ["mp3", "wav", "aac", "flac", "ogg", "m4a"],
+    "documents": ["pdf", "doc", "docx", "txt", "xls", "xlsx", "ppt", "pptx", "odt"],
+    "archives": ["zip", "rar", "tar", "gz", "7z"],
+    "scripts": ["py", "js", "html", "css", "php", "java", "c", "cpp", "sh"]
+}
