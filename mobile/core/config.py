1df437a5e1ee6ba6572d5104e0f3cc3cf60ee752
jo19lune <joachimloick939@gmail.com>
2025-11-04 09:11:38 +0300

    config ameliore

---
+++ mobile/core/config.py
@@ -0,0 +1,10 @@
+EXTENSIONS = {
+    "images": ["jpg", "jpeg", "png", "gif", "bmp", "webp"],
+    "documents": ["pdf", "doc", "docx", "txt", "odt", "rtf", "xls", "xlsx", "ppt", "pptx"],
+    "audio": ["mp3", "wav", "aac", "flac", "ogg", "m4a"],
+    "video": ["mp4", "avi", "mov", "mkv", "wmv", "webm"],
+    "archives": ["zip", "rar", "7z", "tar", "gz"],
+    "code": ["py", "java", "c", "cpp", "js", "html", "css", "php", "rb", "ts"],
+    "fonts": ["ttf", "otf", "woff", "woff2"],
+    "executables": ["exe", "msi", "apk", "bat", "sh"],
+}
